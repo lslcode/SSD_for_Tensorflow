edae69f391dfba18eb489fb92f71a31336b16d3f
lslcode <33273994+lslcode@users.noreply.github.com>
2018-01-17 16:26:50 +0800

    Update ssd300.py

--- ssd300.py
+++ ssd300.py
@@ -31,7 +31,7 @@ class SSD300:
             [1.0, 1.25, 2.0, 3.0]
         ]
         # 最小default box面积比例
-        self.min_box_scale = 0.1
+        self.min_box_scale = 0.05
         # 最大default box面积比例
         self.max_box_scale = 0.9
         # 每个特征层的面积比例
@@ -51,7 +51,7 @@ class SSD300:
         # Batch Normalization 算法的 variance_epsilon 参数
         self.conv_bn_epsilon = 0.00001
         # Jaccard相似度判断阀值
-        self.jaccard_value = 0.7
+        self.jaccard_value = 0.5
 
         # 初始化Tensorflow Graph
         self.generate_graph()
@@ -61,69 +61,72 @@ class SSD300:
         self.input = tf.placeholder(shape=[None, self.img_size[0], self.img_size[1], 3], dtype=tf.float32, name='input_image')
         
         # vvg16卷积层 1
-        self.conv_1_1 = self.convolution(self.input, [3, 3,  3, 64], self.conv_strides_1,'conv_1_1')
-        self.conv_1_2 = self.convolution(self.conv_1_1, [3, 3, 64, 64], self.conv_strides_1,'conv_1_2')
+        self.conv_1_1 = self.convolution(self.input, [3, 3,  3, 16], self.conv_strides_1,'conv_1_1')
+        self.conv_1_2 = self.convolution(self.conv_1_1, [3, 3, 16, 32], self.conv_strides_1,'conv_1_2')
         self.conv_1_2 = tf.nn.max_pool(self.conv_1_2, self.pool_size, self.pool_strides, padding='SAME', name='pool_1_2')
         print('##   conv_1_2 shape: ' + str(self.conv_1_2.get_shape().as_list()))
         # vvg16卷积层 2
-        self.conv_2_1 = self.convolution(self.conv_1_2, [3, 3,  64, 128], self.conv_strides_1,'conv_2_1')
-        self.conv_2_2 = self.convolution(self.conv_2_1, [3, 3, 128, 128], self.conv_strides_1,'conv_2_2')
-        self.conv_2_2 = tf.nn.max_pool(self.conv_2_2, self.pool_size, self.pool_strides, padding='SAME',   name='pool_2_2')
+        self.conv_2_1 = self.convolution(self.conv_1_2, [3, 3,  32, 64], self.conv_strides_1,'conv_2_1')
+        self.conv_2_2 = self.convolution(self.conv_2_1, [3, 3, 64, 128], self.conv_strides_1,'conv_2_2')
+        #self.conv_2_2 = tf.nn.max_pool(self.conv_2_2, self.pool_size, self.pool_strides, padding='SAME',   name='pool_2_2')
         print('##   conv_2_2 shape: ' + str(self.conv_2_2.get_shape().as_list()))
         # vvg16卷积层 3
-        self.conv_3_1 = self.convolution(self.conv_2_2, [3, 3, 128, 256], self.conv_strides_1,'conv_3_1')
-        self.conv_3_2 = self.convolution(self.conv_3_1, [3, 3, 256, 256], self.conv_strides_1,'conv_3_2')
-        self.conv_3_2 = tf.nn.max_pool(self.conv_3_2, self.pool_size, self.pool_strides, padding='SAME', name='pool_3_3')
-        print('##   conv_3_2 shape: ' + str(self.conv_3_2.get_shape().as_list()))
+        self.conv_3_1 = self.convolution(self.conv_2_2, [3, 3, 128, 1], self.conv_strides_1,'conv_3_1')
+        self.conv_3_2 = self.convolution(self.conv_3_1, [1, 1, 1, 1], self.conv_strides_1,'conv_3_2')
+        self.conv_3_3 = self.convolution(self.conv_3_2, [3, 3, 1, 128], self.conv_strides_1,'conv_3_3')
+        self.conv_3_3 = tf.nn.max_pool(self.conv_3_3, self.pool_size, self.pool_strides, padding='SAME', name='pool_3_3')
+        print('##   conv_3_3 shape: ' + str(self.conv_3_3.get_shape().as_list()))
         # vvg16卷积层 4
-        self.conv_4_1 = self.convolution(self.conv_3_2, [3, 3, 256, 512], self.conv_strides_1,'conv_4_1')
-        self.conv_4_2 = self.convolution(self.conv_4_1, [3, 3, 512, 512], self.conv_strides_1,'conv_4_2')
-        #self.conv_4_2 = tf.nn.max_pool(self.conv_4_2, self.pool_size, self.pool_strides, padding='SAME', name='pool_4_3')
-        print('##   conv_4_2 shape: ' + str(self.conv_4_2.get_shape().as_list()))
+        self.conv_4_1 = self.convolution(self.conv_3_3, [3, 3, 128, 1], self.conv_strides_1,'conv_4_1')
+        self.conv_4_2 = self.convolution(self.conv_4_1, [1, 1, 1, 1], self.conv_strides_1,'conv_4_2')
+        self.conv_4_3 = self.convolution(self.conv_4_2, [3, 3, 1, 128], self.conv_strides_1,'conv_4_3')
+        self.conv_4_3 = tf.nn.max_pool(self.conv_4_3, self.pool_size, self.pool_strides, padding='SAME', name='pool_4_3')
+        print('##   conv_4_3 shape: ' + str(self.conv_4_3.get_shape().as_list()))
         # vvg16卷积层 5
-        self.conv_5_1 = self.convolution(self.conv_4_2, [3, 3, 512, 512], self.conv_strides_1,'conv_5_1')
-        self.conv_5_2 = self.convolution(self.conv_5_1, [3, 3, 512, 512], self.conv_strides_1,'conv_5_2')
-        self.conv_5_2 = tf.nn.max_pool(self.conv_5_2, self.pool_size, self.pool_strides, padding='SAME', name='pool_5_3')
-        print('##   conv_5_2 shape: ' + str(self.conv_5_2.get_shape().as_list()))
+        self.conv_5_1 = self.convolution(self.conv_4_3, [3, 3, 128, 1], self.conv_strides_1,'conv_5_1')
+        self.conv_5_2 = self.convolution(self.conv_5_1, [1, 1, 1, 1], self.conv_strides_1,'conv_5_2')
+        self.conv_5_3 = self.convolution(self.conv_5_2, [3, 3, 1, 128], self.conv_strides_1,'conv_5_3')
+        self.conv_5_3 = tf.nn.max_pool(self.conv_5_3, self.pool_size, self.pool_strides, padding='SAME', name='pool_5_3')
+        print('##   conv_5_3 shape: ' + str(self.conv_5_3.get_shape().as_list()))
         # ssd卷积层 6
-        self.conv_6_1 = self.convolution(self.conv_5_2, [3, 3, 512, 1024], self.conv_strides_1,'conv_6_1')
+        self.conv_6_1 = self.convolution(self.conv_5_3, [3, 3, 128, 128], self.conv_strides_1,'conv_6_1')
         print('##   conv_6_1 shape: ' + str(self.conv_6_1.get_shape().as_list()))
         # ssd卷积层 7 
-        self.conv_7_1 = self.convolution(self.conv_6_1, [1, 1, 1024, 1024], self.conv_strides_1,'conv_7_1')
+        self.conv_7_1 = self.convolution(self.conv_6_1, [1, 1, 128, 128], self.conv_strides_1,'conv_7_1')
         print('##   conv_7_1 shape: ' + str(self.conv_7_1.get_shape().as_list()))
         # ssd卷积层 8     
-        self.conv_8_1 = self.convolution(self.conv_7_1, [1, 1, 1024, 256], self.conv_strides_1,'conv_8_1')
-        self.conv_8_2 = self.convolution(self.conv_8_1, [3, 3,  256, 512], self.conv_strides_2,'conv_8_2')
+        self.conv_8_1 = self.convolution(self.conv_7_1, [1, 1, 128, 128], self.conv_strides_1,'conv_8_1')
+        self.conv_8_2 = self.convolution(self.conv_8_1, [3, 3, 128, 128], self.conv_strides_2,'conv_8_2')
         print('##   conv_8_2 shape: ' + str(self.conv_8_2.get_shape().as_list()))
         # ssd卷积层 9   
-        self.conv_9_1 = self.convolution(self.conv_8_2, [1, 1, 512, 128], self.conv_strides_1,'conv_9_1')
-        self.conv_9_2 = self.convolution(self.conv_9_1, [3, 3, 128, 256], self.conv_strides_2,'conv_9_2')
+        self.conv_9_1 = self.convolution(self.conv_8_2, [1, 1, 128, 128], self.conv_strides_1,'conv_9_1')
+        self.conv_9_2 = self.convolution(self.conv_9_1, [3, 3, 128, 128], self.conv_strides_2,'conv_9_2')
         print('##   conv_9_2 shape: ' + str(self.conv_9_2.get_shape().as_list()))
         # ssd卷积层 10    
-        self.conv_10_1 = self.convolution(self.conv_9_2, [1, 1, 256, 128], self.conv_strides_1,'conv_10_1')
-        self.conv_10_2 = self.convolution(self.conv_10_1, [3, 3, 128, 256], self.conv_strides_2,'conv_10_2')
+        self.conv_10_1 = self.convolution(self.conv_9_2, [1, 1, 128, 128], self.conv_strides_1,'conv_10_1')
+        self.conv_10_2 = self.convolution(self.conv_10_1, [3, 3, 128, 128], self.conv_strides_2,'conv_10_2')
         print('##   conv_10_2 shape: ' + str(self.conv_10_2.get_shape().as_list()))
         # ssd卷积层 11
-        self.conv_11 = tf.nn.max_pool(self.conv_10_2, self.pool_size, self.pool_strides, "VALID")
+        self.conv_11 = tf.nn.avg_pool(self.conv_10_2, self.pool_size, self.pool_strides, "VALID")
         print('##   conv_11 shape: ' + str(self.conv_11.get_shape().as_list()))
 
-        # 第 1 层 特征层，来源于conv_4_2  
-        self.features_1 = self.convolution(self.conv_4_2, [3, 3, 512, self.default_box_size[0] * (self.classes_size + 4)], self.conv_strides_1,'features_1')
+        # 第 1 层 特征层，来源于conv_4_3  
+        self.features_1 = self.convolution(self.conv_4_3, [3, 3, 128, self.default_box_size[0] * (self.classes_size + 4)], self.conv_strides_1,'features_1')
         print('##   features_1 shape: ' + str(self.features_1.get_shape().as_list()))
         # 第 2 层 特征层，来源于conv_7_1
-        self.features_2 = self.convolution(self.conv_7_1, [3, 3, 1024, self.default_box_size[1] * (self.classes_size + 4)], self.conv_strides_1,'features_2')
+        self.features_2 = self.convolution(self.conv_7_1, [3, 3, 128, self.default_box_size[1] * (self.classes_size + 4)], self.conv_strides_1,'features_2')
         print('##   features_2 shape: ' + str(self.features_2.get_shape().as_list()))
         # 第 3 层 特征层，来源于conv_8_2
-        self.features_3 = self.convolution(self.conv_8_2, [3, 3, 512,  self.default_box_size[2] * (self.classes_size + 4)], self.conv_strides_1,'features_3')
+        self.features_3 = self.convolution(self.conv_8_2, [3, 3, 128,  self.default_box_size[2] * (self.classes_size + 4)], self.conv_strides_1,'features_3')
         print('##   features_3 shape: ' + str(self.features_3.get_shape().as_list()))
         # 第 4 层 特征层，来源于conv_9_2
-        self.features_4 = self.convolution(self.conv_9_2, [3, 3, 256,  self.default_box_size[3] * (self.classes_size + 4)], self.conv_strides_1,'features_4')
+        self.features_4 = self.convolution(self.conv_9_2, [3, 3, 128,  self.default_box_size[3] * (self.classes_size + 4)], self.conv_strides_1,'features_4')
         print('##   features_4 shape: ' + str(self.features_4.get_shape().as_list()))
         # 第 5 层 特征层，来源于conv_10_2
-        self.features_5 = self.convolution(self.conv_10_2, [3, 3, 256,  self.default_box_size[4] * (self.classes_size + 4)], self.conv_strides_1,'features_5')
+        self.features_5 = self.convolution(self.conv_10_2, [3, 3, 128,  self.default_box_size[4] * (self.classes_size + 4)], self.conv_strides_1,'features_5')
         print('##   features_5 shape: ' + str(self.features_5.get_shape().as_list()))
         # 第 6 层 特征层，来源于conv_11
-        self.features_6 = self.convolution(self.conv_11, [1, 1, 256,  self.default_box_size[5] * (self.classes_size + 4)], self.conv_strides_1,'features_6')
+        self.features_6 = self.convolution(self.conv_11, [1, 1, 128,  self.default_box_size[5] * (self.classes_size + 4)], self.conv_strides_1,'features_6')
         print('##   features_6 shape: ' + str(self.features_6.get_shape().as_list()))
         
         # 特征层集合
@@ -173,7 +176,7 @@ class SSD300:
 
     # 图像检测与训练
     # input_images : 输入图像数据，格式:[None,width,hight,channel]
-    # actual_data : 标注数据，格式:[None,[None,top_X,top_Y,width,hight,classes]] , classes值范围[0,classes_size)
+    # actual_data : 标注数据，格式:[None,[None,center_X,center_Y,width,hight,classes]] , classes值范围[0,classes_size)
     def run(self, input_images, actual_data):
         # 训练部分
         if self.isTraining :
@@ -266,7 +269,7 @@ class SSD300:
             bias = tf.get_variable(initializer=tf.random_normal(shape[-1:], 0, 1), dtype=tf.float32, name=name+'_bias')
             result = tf.nn.conv2d(input, weight, strides, padding='SAME', name=name+'_conv')
             result = tf.nn.bias_add(result, bias)
-            result = self.batch_normalization(result, name=name+'_batch_normal')
+            result = self.batch_normalization(result, name=name+'_bn')
             result = tf.nn.relu(result, name=name+'_relu')
             return result
 
@@ -286,16 +289,16 @@ class SSD300:
     def batch_normalization(self, input, name):
         with tf.variable_scope(name):
             bn_input_shape = input.get_shape() 
-            moving_mean = tf.get_variable('bn_mean', bn_input_shape[-1:] , initializer=tf.zeros_initializer, trainable=False)
-            moving_variance = tf.get_variable('bn_variance', bn_input_shape[-1:] , initializer=tf.ones_initializer, trainable=False)
+            moving_mean = tf.get_variable(name+'_mean', bn_input_shape[-1:] , initializer=tf.zeros_initializer, trainable=False)
+            moving_variance = tf.get_variable(name+'_variance', bn_input_shape[-1:] , initializer=tf.ones_initializer, trainable=False)
             def mean_var_with_update():
-                mean, variance = tf.nn.moments(input, list(range(len(bn_input_shape) - 1)), name='bn_moments')
+                mean, variance = tf.nn.moments(input, list(range(len(bn_input_shape) - 1)), name=name+'_moments')
                 with tf.control_dependencies([assign_moving_average(moving_mean, mean, self.conv_bn_decay),assign_moving_average(moving_variance, variance, self.conv_bn_decay)]):
                     return tf.identity(mean), tf.identity(variance)
             #mean, variance = tf.cond(tf.cast(self.isTraining, tf.bool), mean_var_with_update, lambda: (moving_mean, moving_variance))
             mean, variance = tf.cond(tf.cast(True, tf.bool), mean_var_with_update, lambda: (moving_mean, moving_variance))
-            beta = tf.get_variable('bn_beta', bn_input_shape[-1:] , initializer=tf.zeros_initializer)
-            gamma = tf.get_variable('bn_gamma', bn_input_shape[-1:] , initializer=tf.ones_initializer)
+            beta = tf.get_variable(name+'_beta', bn_input_shape[-1:] , initializer=tf.zeros_initializer)
+            gamma = tf.get_variable(name+'_gamma', bn_input_shape[-1:] , initializer=tf.ones_initializer)
             return tf.nn.batch_normalization(input, mean, variance, beta, gamma, self.conv_bn_epsilon)
     
     # smooth_L1 算法
@@ -308,18 +311,17 @@ class SSD300:
         # 用于后续的jaccard匹配
         all_default_boxes = []
         for index, map_shape in zip(range(len(self.feature_maps_shape)), self.feature_maps_shape):
-            width = map_shape[1]
-            height = map_shape[2]
-            scale = self.default_box_scale[index]
-            ratios = self.box_aspect_ratio[index]
+            width = int(map_shape[1])
+            height = int(map_shape[2])
+            cell_scale = self.default_box_scale[index]
             for x in range(width):
                 for y in range(height):
-                    for i,ratio in zip(range(len(ratios)), ratios):
-                        top_x = x / float(width)
-                        top_y = y / float(height)
-                        box_width = np.sqrt(scale * ratio)
-                        box_height = np.sqrt(scale / ratio)
-                        all_default_boxes.append([top_x, top_y, box_width, box_height])
+                    for ratio in self.box_aspect_ratio[index]:
+                        center_x = (x / float(width)) + (0.5/ float(width))
+                        center_y = (y / float(height)) + (0.5 / float(height))
+                        box_width = np.sqrt(cell_scale * ratio)
+                        box_height = np.sqrt(cell_scale / ratio)
+                        all_default_boxes.append([center_x, center_y, box_width, box_height])
         all_default_boxes = np.array(all_default_boxes)
         #检查数据是否正确
         all_default_boxes = self.check_numerics(all_default_boxes,'all_default_boxes') 
@@ -334,7 +336,7 @@ class SSD300:
         gt_positives_jacc = np.zeros((input_actual_data_len, self.all_default_boxs_len))
         gt_positives = np.zeros((input_actual_data_len, self.all_default_boxs_len))
         gt_negatives = np.zeros((input_actual_data_len, self.all_default_boxs_len))
-        background_jacc = max(0, (self.jaccard_value-0.3))
+        background_jacc = max(0, (self.jaccard_value-0.2))
         # 初始化正例训练数据
         for img_index in range(input_actual_data_len):
             for pre_actual in input_actual_data[img_index]:
@@ -350,6 +352,7 @@ class SSD300:
                         gt_negatives[img_index][boxe_index] = 0
             # 如果没有正例，则随机创建一个正例，预防nan
             if np.sum(gt_positives[img_index])==0 :
+                #print('【没有匹配jacc】:'+str(input_actual_data[img_index]))
                 random_pos_index = np.random.randint(low=0, high=self.all_default_boxs_len, size=1)[0]
                 gt_class[img_index][random_pos_index] = self.background_classes_val
                 gt_location[img_index][random_pos_index] = [0,0,0,0]
@@ -358,8 +361,6 @@ class SSD300:
                 gt_negatives[img_index][random_pos_index] = 0
             # 正负例比值 1:3
             gt_neg_end_count = int(np.sum(gt_positives[img_index]) * 3)
-            if np.isnan(gt_neg_end_count) or gt_neg_end_count==0:
-                gt_neg_end_count = 1
             if (gt_neg_end_count+np.sum(gt_positives[img_index])) > self.all_default_boxs_len :
                 gt_neg_end_count = self.all_default_boxs_len - np.sum(gt_positives[img_index])
             # 随机选择负例
@@ -372,13 +373,26 @@ class SSD300:
         return gt_class, gt_location, gt_positives, gt_negatives
 
     # jaccard算法
-    # 计算IOU，rect1、rect2格式为[top_x,top_y,width,height]           
+    # 计算IOU，rect1、rect2格式为[center_x,center_y,width,height]           
     def jaccard(self, rect1, rect2):
-        x_overlap = max(0, (min(rect1[0]+rect1[2], rect2[0]+rect2[2]) - max(rect1[0], rect2[0])))
-        y_overlap = max(0, (min(rect1[1]+rect1[3], rect2[1]+rect2[3]) - max(rect1[1], rect2[1])))
+        x_overlap = max(0, (min(rect1[0]+(rect1[2]/2), rect2[0]+(rect2[2]/2)) - max(rect1[0]-(rect1[2]/2), rect2[0]-(rect2[2]/2))))
+        y_overlap = max(0, (min(rect1[1]+(rect1[3]/2), rect2[1]+(rect2[3]/2)) - max(rect1[1]-(rect1[3]/2), rect2[1]-(rect2[3]/2))))
         intersection = x_overlap * y_overlap
-        area_box_a = rect1[2] * rect1[3]
-        area_box_b = rect2[2] * rect2[3]
+        # 删除超出图像大小的部分
+        rect1_width_sub = 0
+        rect1_height_sub = 0
+        rect2_width_sub = 0
+        rect2_height_sub = 0
+        if (rect1[0]-rect1[2]/2) < 0 : rect1_width_sub += 0-(rect1[0]-rect1[2]/2)
+        if (rect1[0]+rect1[2]/2) > 1 : rect1_width_sub += (rect1[0]+rect1[2]/2)-1
+        if (rect1[1]-rect1[3]/2) < 0 : rect1_height_sub += 0-(rect1[1]-rect1[3]/2)
+        if (rect1[1]+rect1[3]/2) > 1 : rect1_height_sub += (rect1[1]+rect1[3]/2)-1
+        if (rect2[0]-rect2[2]/2) < 0 : rect2_width_sub += 0-(rect2[0]-rect2[2]/2)
+        if (rect2[0]+rect2[2]/2) > 1 : rect2_width_sub += (rect2[0]+rect2[2]/2)-1
+        if (rect2[1]-rect2[3]/2) < 0 : rect2_height_sub += 0-(rect2[1]-rect2[3]/2)
+        if (rect2[1]+rect2[3]/2) > 1 : rect2_height_sub += (rect2[1]+rect2[3]/2)-1
+        area_box_a = (rect1[2]-rect1_width_sub) * (rect1[3]-rect1_height_sub)
+        area_box_b = (rect2[2]-rect2_width_sub) * (rect2[3]-rect2_height_sub)
         union = area_box_a + area_box_b - intersection
         if intersection > 0 and union > 0 : 
             return intersection / union 
@@ -398,4 +412,3 @@ class SSD300:
                 data_error = '【'+ message +'】出现数据错误！【nan：'+str(nan_count)+'|inf：'+str(inf_count)+'|-inf：'+str(n_inf_count)+'】'
                 raise Exception(data_error) 
         return  input_dataset
-    
